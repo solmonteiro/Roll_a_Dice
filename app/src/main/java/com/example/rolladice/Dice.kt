4af3b0f1ea50ee55de4cc69f8650c36d8e2f2ebd
solmonteiro <sole7mares@gmail.com>
2022-08-04 10:41:18 -0300

    Comments added

--- app/src/main/java/com/example/rolladice/Dice.kt
+++ app/src/main/java/com/example/rolladice/Dice.kt
@@ -1,5 +1,10 @@
 package com.example.rolladice
 
+/**
+ * This class allows to create an dice object with one propertie and one method.
+ *
+ */
+
 class Dice (private val numberSide:Int){
 
     fun roll(): Int {
--- app/src/main/java/com/example/rolladice/MainActivity.kt
+++ app/src/main/java/com/example/rolladice/MainActivity.kt
@@ -5,6 +5,9 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This class allows user to roll a dice through a button on screen
+ */
 class MainActivity : AppCompatActivity() {
 
 
@@ -14,14 +17,16 @@ class MainActivity : AppCompatActivity() {
 
         val bt_roll: Button = findViewById(R.id.bt_roll)
 
-        bt_roll.setOnClickListener {
-            rollDice()
-        }
+        bt_roll.setOnClickListener { rollDice() }
     }
 
+    /**
+     * It creates a dice and, roll it and show the result to the user
+     */
     private fun rollDice() {
         val dice = Dice(6)
         val resultNumber = dice.roll()
+        //TextView instantied here
         val tv_number: TextView = findViewById(R.id.tv_number)
         tv_number.text = resultNumber.toString()
     }
