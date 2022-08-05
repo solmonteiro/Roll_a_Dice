75613ebf47b7cd6c46102dee12ac911c69fe132c
solmonteiro <sole7mares@gmail.com>
2022-08-05 08:55:15 -0300

    Dice roller creation finished

--- app/src/main/java/com/example/rolladice/MainActivity.kt
+++ app/src/main/java/com/example/rolladice/MainActivity.kt
@@ -18,26 +18,34 @@ class MainActivity : AppCompatActivity() {
         val bt_roll: Button = findViewById(R.id.bt_roll)
 
         bt_roll.setOnClickListener { rollDice() }
+
+        //Do a dice roll when the app starts
+        rollDice()
     }
 
+
+
     /**
-     * It creates 2 dices and, roll it and show the result to the user
+     * It creates 1 dice and, roll it and show the result to the user
      */
     private fun rollDice() {
         val dice = Dice(6)
-        var resultNumber = dice.roll()
+        val resultNumber = dice.roll()
         //ImageView instantied here
         val diceImage: ImageView = findViewById(R.id.imgView)
 
-        when (resultNumber) {
-            1 -> diceImage.setImageResource(R.drawable.dice_1)
-            2 -> diceImage.setImageResource(R.drawable.dice_2)
-            3 -> diceImage.setImageResource(R.drawable.dice_3)
-            4 -> diceImage.setImageResource(R.drawable.dice_4)
-            5 -> diceImage.setImageResource(R.drawable.dice_5)
-            6 -> diceImage.setImageResource(R.drawable.dice_6)
+//Define the rool image to show according with te number
+        val drawableResource = when (resultNumber) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
 
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = (resultNumber.toString())
 
     }
 
